34fbb5ac4d08b2e9c3657415ad4ebfda406b9bf5
DudaBertole <duda.bertole@hotmail.com>
2026-04-26 10:29:38 -0300

    feat: initial project setup
    - add Maven dependencies to pom.xml
    - create initial OpenAPI specification file

---
+++ src/main/java/me/dudabertole/unidoc/UniDocApplication.java
@@ -0,0 +1,13 @@
+package me.dudabertole.unidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniDocApplication.class, args);
+    }
+
+}
